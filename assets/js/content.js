9cd90ec2ef27a0b6adab8bc3f950ea5426d125d5
Money Maker <mitchell.harris@westnet.com.au>
2022-09-22 22:09:19 +0800

    added js for business news section, added some minor html and css

--- assets/js/content.js
+++ assets/js/content.js
@@ -25,5 +25,50 @@
 
 
 // create individual news link cards
+
+// Assuming the layout of HTML is 
+/* 
+<div class="business-news-card">
+    <h1 class="business-news-title">Business News</h1>
+    <div class="business-news-container"></div>
+</div> 
+*/
+const newsContainer = $('.business-news-container');
+const newsAPIKey = "28be02997c3a44bcaf523fdb51d44ec3"
+$(function createNewsContainers(){
+
+    var newsQueryURL = "https://newsapi.org/v2/top-headlines?country=au&category=business&apiKey=" + newsAPIKey
+    console.log(newsQueryURL);
+
+    return fetch(newsQueryURL)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(result){
+        for(let i=0; i<=5; i++){
+            console.log(result);
+            let image = result.articles[i].urlToImage;
+            let title = result.articles[i].title;
+            let description = result.articles[i].description;
+            let link = result.articles[i].url; 
+            
+            var article = document.createElement("div");
+            article.classList.add("articles-class");
+            article.innerHTML = `
+            <div class="article-image-container">
+            <img src="${image}" class="article-image" alt="${title}">
+            </div>
+            <div class="article-text">
+            <a href="${link}" title="Go to the website" target="_blank class="article-title">${title}
+            <h3 class="article-description">${description}
+            </div>`
+
+            newsContainer.append(article);
+        };
+        
+    });
+});
+
 // in those cards, include an image and description of the news article (depends on what the API will show)
 // when a user clicks on a news link card, take them to a new tab and open the link
